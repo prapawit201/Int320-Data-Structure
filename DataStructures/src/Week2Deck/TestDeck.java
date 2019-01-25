0e833835f5dd8d0c746233157ea8c20ddca0825a
zBabEs <32611645+prapawit201@users.noreply.github.com>
2019-01-25 12:17:09 +0700

    change more easy

--- DataStructures/src/Week2Deck/TestDeck.java
+++ DataStructures/src/Week2Deck/TestDeck.java
@@ -16,13 +16,21 @@ public class TestDeck {
     public static void main(String[] args) {
         Deck d = new Deck();
         d.randomOrder();
+        int pokcount = 0;
         for (int i = 1; i <= 10; i++) {
             List<Card> cardOnhands = d.getCardsOnHand(2);
             int value = cardOnhands.get(0).getValue() + cardOnhands.get(1).getValue();
-            String pok = (value % 10 == 8 || value % 10 == 9 ? " -> POK" : "");
-            System.out.println("[" + i + "] " + cardOnhands + pok);
+            String pok = "";
+            if (value % 10 == 8 || value % 10 == 9 ) {
+                  
+                  pok = " --> POK";
+                  pokcount++;
+            }
+            System.out.println("{"+i+"}"+cardOnhands+pok);
         }
         System.out.println("--------------------");
         System.out.println(d.getCards());
+        System.out.println();
+        System.out.println("Pokcount : "+pokcount);
     }
 }
